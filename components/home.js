7eec21d7c9643ea54f38e172a1f6ec78b76a4d95
daniel235 <daduncan298@gmail.com>
2019-02-12 01:36:26 -0600

    update

--- components/home.js
+++ components/home.js
@@ -3,10 +3,15 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { loggedIn: false };
+    }
     render() {
         return(
-            <View>
-                <Text>Home Screen</Text>
+            <View style={styles.container}>
+                <Text>Your tasks</Text>
+                
                 <Button
                     title="Go to profile"
                     onPress={() => this.props.navigation.navigate('Profile')}
