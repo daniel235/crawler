ef12c310cd753200121e4e8471a245329be38d3b
daniel235 <daduncan298@gmail.com>
2019-02-25 15:10:30 -0600

    added firebase authentication(still working on it)

---
+++ cloud/public/js/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const app = express();
+
+admin.initializeApp();
+
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+const authenticate = async (req, res, next) => {
+    if(!req.headers.authorization || !req.headers.startsWith('Bearer')){
+        res.status(403).send('Unauthorized');
+        return;
+    }
+    const idToken = req.headers.authorization.split('Bearer ')[1];
+    try {
+        const decodeIdToken = await admin.auth().verifyIdToken(idToken);
+        req.user = decodeIdToken;
+        next();
+        return;
+    }
+    catch(e) {
+        res.status(403).send('Unauthorized');
+        return;
+    }
+}
+
+app.use(authenticate);
